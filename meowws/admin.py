dbd3801bbadd1f60b68881fa0335a438ccb45738
krishnu9 <krishnu.binod@gmail.com>
2020-07-20 02:03:43 +0530

    Add Like field

--- meowws/admin.py
+++ meowws/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Meoww
+from .models import Meoww, MeowwLike
+
+
+class MeowwLikeAdmin(admin.TabularInline):
+    model = MeowwLike
 
 
 class MeowwAdmin(admin.ModelAdmin):
+    inlines = [MeowwLikeAdmin]
     list_display = ['__str__', 'user']
     search_fields = ['content', 'user__username', 'user__email']
 
--- meowws/models.py
+++ meowws/models.py
@@ -5,12 +5,21 @@ import random
 User = settings.AUTH_USER_MODEL
 
 
+class MeowwLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    meoww = models.ForeignKey("Meoww", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
 class Meoww(models.Model):
     # id = models.AutoField(primary_key=True)
     # A User can have many tweets
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(
+        User, related_name='tweet_user', blank=True, through=MeowwLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.content
